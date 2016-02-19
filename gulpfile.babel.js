e59e4ca777b67657291aecea1498f6d43189ce23
sky <2813402@qq.com>
2016-02-19 16:57:52 +0800

    123

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,7 +14,7 @@ import buffer from 'vinyl-buffer';
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = true;
 
 const AUTOPREFIXER_BROWSERS = [
   'ie >= 9',
